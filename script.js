b9bd84815b7dc8b5a43af0275cbb606a4d7e9572
Sander Smedbol <sander.smedbol@stud.noroff.no>
2022-01-05 12:16:22 +0100

    lesson task 2

---
+++ script.js
@@ -0,0 +1,10 @@
+var animal = "frog";
+var delivered = true;
+var score = 12;
+var firstName = "John";
+var lastName = "Smith";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+console.log(typeof fullName);
+console.log(typeof delivered);
