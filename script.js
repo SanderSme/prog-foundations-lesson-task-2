aba7d3704722476d73eb7b89240201be63294ad0
Sander Smedbol <sander.smedbol@stud.noroff.no>
2022-01-06 13:53:37 +0100

    ..

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var animal = "frog";
+console.log(animal);
 var delivered = true;
+console.log(delivered);
 var score = 12;
+console.log(score);
 var firstName = "John";
 var lastName = "Smith";
 var fullName = firstName + " " + lastName;
